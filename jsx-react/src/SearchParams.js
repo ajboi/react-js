01b8beb3b5983639aa8917cb5cbf95be260e8e51
ajboi <aj.kannan1101@gmail.com>
2021-05-07 18:01:45 +0530

    Implemented useEffect hook.

--- jsx-react/src/SearchParams.js
+++ jsx-react/src/SearchParams.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./UseDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA"); //If statements never go inside of conditionals and loops.
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  console.log(useDropdown("Animal", "dog", ANIMALS));
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]); //cleaning up existing before updating.
+    setBreed(""); //cleaning up existing before updating.
+
+    pet.breeds(animal).then(
+      ({ breeds }) => {
+        const breedStrings = breeds.map(({ name }) => name);
+        setBreeds(breedStrings);
+      },
+      (error) => console.error(`error ${error}`) // Error is also logged inside then because all network calls always resolve, if i am correct.
+    );
+  }, [animal, setBreed, setBreeds]);
+
+  // pet.breeds("dog").then((result) => {
+  //   const x = result.breeds.map((breed) => breed.name);
+  //   console.log(x);
+  // });
+
   return (
     <div className="search-params">
       <form>
